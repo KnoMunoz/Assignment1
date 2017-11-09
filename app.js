a537f5af6f01edafbc4921ac7ab4b7d61237e3e5
KnoMunoz <32555612+KnoMunoz@users.noreply.github.com>
2017-11-09 10:50:02 -0300

    First Version Assignment 1

---
+++ app.js
@@ -0,0 +1,37 @@
+(function() {
+	'use strict';
+
+	angular.module('LunchCheckApp', []).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = [ '$scope' ];
+	function LunchCheckController($scope) {
+		$scope.mealList = "";
+		$scope.toMuchMessage = "";
+
+		$scope.checkTooMuch = function() {
+			if($scope.mealList == "") {
+				$scope.toMuchMessage = "Please enter data first!";
+			} else {
+				$scope.toMuchMessage = "";
+				var arrayOfMeals = getItems($scope.mealList);
+				if(arrayOfMeals.length >= 4) {
+					$scope.toMuchMessage = "Too much!";
+				} else {
+					$scope.toMuchMessage = "Enjoy!";
+				}
+			}
+		};
+		
+		/*
+		 * getItems method uses filter function to delete empty elements
+		 */
+		function getItems(mealList) {
+			var arrayOfMeals = mealList.split(",");
+			var newArrayOfMeals = arrayOfMeals.filter(function(meal) {
+				return meal.trim().length > 0;
+			})
+			return newArrayOfMeals;
+		};
+	};
+
+})();
